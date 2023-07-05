74eab4f5e2943fd0aa541d81b683f23b0196b3e5
chic <thiagoiklock@gmail.com>
2023-07-05 12:39:31 -0300

    cap 2

---
+++ etapa01/cap02/exer02/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(`Sabemso que na mbase 10 o maior número de um digíto é 9 e o maior dos núemors é 99. ... qual oe omaior número de oito digítos?`)
+
+	fmt.Println("Bae 10 = 10^8-1 =", 100000000-1)
+}
