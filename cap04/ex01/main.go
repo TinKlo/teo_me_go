354acd982235305d1f484980f069ae4c93901ee5
chic <thiagoiklock@gmail.com>
2023-07-08 21:49:05 -0300

    finishes cap 04

---
+++ cap04/ex01/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("O que o Programa a seguir exibe?")
+	i := 10
+	if i > 10 {
+		fmt.Println("Big")
+	} else {
+		fmt.Println("Small")
+	}
+}
